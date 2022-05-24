890ff20750b8ebb2f8b66229712b133701e90702
ldh3907 <ldh165163@gmail.com>
2022-05-24 11:32:26 +0900

    2022-05-24

--- 데이터 타입/dataType.js
+++ 데이터 타입/dataType.js
@@ -41,3 +41,28 @@ if (user !== user2) {
 
 console.log(user.name, user2.name);
 console.log(user === user2);
+
+var c;
+console.log(c);
+
+var obj3 = {
+  a: 1,
+};
+
+console.log(obj3.a);
+console.log(obj3.b);
+console.log(b);
+
+var func = function () {};
+var d = func();
+console.log(d);
+
+var arr1 = [];
+arr1.length = 3;
+console.log(arr1);
+
+var arr2 = new Array(3);
+console.log(arr2);
+
+var arr3 = [undefined, undefined, undefined];
+console.log(arr3);
