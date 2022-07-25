f54dae3009c5867c9232ec3a9b3dbee5eed302be
ldh3907 <ldh165163@gmail.com>
2022-07-26 00:40:43 +0900

    20220726

--- 김민태의 프론트엔드 아카데미/참조사전/인터페이스와 타입/index.ts
+++ 김민태의 프론트엔드 아카데미/참조사전/인터페이스와 타입/index.ts
@@ -4,7 +4,7 @@ const foo: allTypes.FooFunction = function () {
   return "아무 쓸모 없는 함수";
 };
 
-const iUser: allTypes.IUser = {
+const iUser: allTypes.TUser = {
   id: 1,
   name: "빌게이츠",
   email: "bill@ms.com",
@@ -12,7 +12,7 @@ const iUser: allTypes.IUser = {
   active: "y",
 };
 
-const iUserProfile: allTypes.IUserProfile = {
+const iUserProfile: allTypes.TUserProfile = {
   id: 1,
   name: "빌게이츠",
   email: "bill@ms.com",
@@ -22,6 +22,44 @@ const iUserProfile: allTypes.IUserProfile = {
   github: "okay",
 };
 
-const IUserProfiles: allTypes.IUserProfile[] = [];
+const IUserProfiles: allTypes.TUserProfile[] = [];
 
-const iStyle : allTypes.
+const iStyle: allTypes.IOnlyNumberValueObject = {
+  borderWidth: 5,
+  width: 300,
+  height: 100,
+};
+
+const tStyle: allTypes.TOnlyBooleanValueObject = {
+  border: true,
+  visible: false,
+  display: true,
+};
+
+const getApi: allTypes.IGetApi = (url, search = "") => {
+  return new Promise((resolve) => resolve);
+};
+
+class Rect implements allTypes.IRect {
+  //   private id: number;
+  public id: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.id = Math.random() * 100000;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+function createDefaultRect(cstor: allTypes.IRectConstrunctor) {
+  return new cstor(0, 0, 100, 100);
+}
+
+const rect1 = new Rect(0, 0, 100, 200);
+const rect2 = createDefaultRect(Rect);
--- 김민태의 프론트엔드 아카데미/참조사전/인터페이스와 타입/type.ts
+++ 김민태의 프론트엔드 아카데미/참조사전/인터페이스와 타입/type.ts
@@ -36,11 +36,76 @@ export type TUser = {
   active: YesOrNo;
 };
 
-export type TUser = {
-  address?: string;
-};
+// export type TUser = {
+//   address?: string;
+// };
 
 export interface IUserProfile extends IUser {
   profileImage: string;
   github?: string;
+  twitter?: string;
+}
+
+export type TUserProfile = IUser & {
+  profileImage: string;
+  github?: string;
+  twitter?: string;
+};
+
+export interface Color {
+  fontColor: string;
+  strokeColor: string;
+  borderColor: string;
+  backgroundColor: string;
+}
+
+export type Display = {
+  display: "none" | "block";
+  visiblity: boolean;
+  opacity: number;
+};
+
+export type Geometry = {
+  width: number;
+  height: number;
+  padding: number;
+  margin: number;
+};
+
+export interface IStyle extends Color, Display, Geometry {
+  tagName: string;
+}
+
+export type TStyle = Color &
+  Display &
+  Geometry & {
+    tagName: string;
+  };
+
+export interface IOnlyNumberValueObject {
+  [key: string]: number;
+}
+
+export type TOnlyBooleanValueObject = {
+  [prop: string]: boolean;
+};
+
+export interface IGetApi {
+  (url: string, search?: string): Promise<string>;
+}
+
+export type TGetApi = {
+  (url: string, search?: string): Promise<string>;
+};
+
+export interface IRect {
+  id: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface IRectConstrunctor {
+  new (x: number, y: number, width: number, height: number): IRect;
 }
